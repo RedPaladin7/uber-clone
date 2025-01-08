812c3ce067b331f330b3d0866fa34887a8ef0b93
RedPaladin7 <ishans7325@gmail.com>
2025-01-08 19:22:50 +0530

    profile and logout routes

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 dotenv.config()
 const connectToDb = require('./db/db.js')
 const userRoutes = require('./routes/user.routes.js')
+const cookieParser = require('cookie-parser')
 
 connectToDb()
 
@@ -11,6 +12,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser)
 
 app.get('/', (req, res)=>{
     res.send('Hello, world')
---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+const userModel = require('../models/user.model')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// only showing the profile of the user which is logged in
+
+module.exports.authUser = async(req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1]
+    if(!token){
+        return res.status(401).json({message: 'Unauthorized'})
+    }
+
+    const isBlacklisted = await userModel.findOne({token: token})
+    // although we cleared cookies after logging out 
+    // user might have saved it from local storage
+    // hence we need to blacklist it
+
+    if(isBlacklisted){
+        return res.status(401).json({message: 'Unauthorized'})
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        // while encoding we passed only the id of the user 
+        // so we get that in the decoded variable
+        const user = await userModel.findById(decoded._id)
+        req.user = user
+        //  now the request has info about the user
+        return next()
+    } catch (error) {
+        return res.status(401).json({message: 'Unauthorized'})
+    }
+}
